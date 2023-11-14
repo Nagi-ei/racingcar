5c00a2fcaa26075e2c99a2a94d9c87ca29132a2e
Nagi <nakt4453@gmail.com>
2023-11-15 01:27:49 +0900

    docs: 기능목록 추가

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    "plugin:airbnb-base/recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
